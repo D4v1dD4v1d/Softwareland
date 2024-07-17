1c00d41eb0fbc928145a612e37d0b3d88385aeb0
David <david.pz.zv@gmail.com>
2024-07-16 18:43:12 -0600

    Practica #1

---
+++ src/components/titulo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/titulo.css';
+function titulo() {
+  return(
+    <div className='titulo'>
+        <h1>PRACTICA 1</h1>
+    </div>
+  ); 
+}
+
+export default titulo;
