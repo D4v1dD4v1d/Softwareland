3643555e663b2d385bfea2e5d86121a52e8c4489
D4v1dD4v1d <david.pz.zv@gmail.com>
2024-07-31 16:05:41 -0600

    Practica #2 - Endpoints

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3002;
 
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta básica para la solicitud raíz ('/')
 app.get('/', (req, res) => {
   const { name, lastName } = req.query;
   if (name && lastName) {
@@ -11,6 +15,32 @@ app.get('/', (req, res) => {
   }
 });
 
+// Ruta para manejar solicitudes GET a /users
+app.get('/users', (req, res) => {
+  res.send('GET request to /users');
+});
+
+// Ruta para manejar solicitudes POST a /users
+app.post('/users', (req, res) => {
+  res.send('POST request to /users');
+});
+
+// Ruta para manejar solicitudes PUT a /users
+app.put('/users', (req, res) => {
+  res.send('PUT request to /users');
+});
+
+// Ruta para manejar solicitudes DELETE a /users
+app.delete('/users', (req, res) => {
+  res.send('DELETE request to /users');
+});
+
+// Middleware para manejar errores 404 para cualquier otra ruta
+app.use((req, res) => {
+  res.status(404).send('Error 404: Ruta no encontrada');
+});
+
+// Inicio del servidor
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
